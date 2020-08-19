1cbe612c6ce46d9611e5f991a697a5eab96a408b
jack <240102906@qq.com>
2020-08-19 11:55:42 +0800

    最后修改记录

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="hello">
-<<<<<<< HEAD
      <p>这是wsh修改的一个记录</p>
      <span>这是又一次修改记录！</span>
-=======
     <h1>我是组员之一</h1>
     <h2>组长最鳖孙</h2>
->>>>>>> brh
   </div>
 </template>
 
@@ -15,12 +12,9 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-<<<<<<< HEAD
-      msg: 'Welcome to Your Vue.js App',
-      msg5:'王少华'
-=======
+      msg1: 'Welcome to Your Vue.js App',
+      msg5:'王少华',
       msg:'白二慧'
->>>>>>> brh
     }
   }
 }
